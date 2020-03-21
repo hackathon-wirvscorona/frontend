b449ccaef7f292d868626fe618cf187adba37217
Felix Kueper <uhuun@student.kit.edu>
2020-03-21 19:20:27 +0100

    more changes to style and top bar

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-top-bar',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent implements OnInit {
+  offsetmargin = 0;
+  sticky = 0;
+  navbar = null;
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.navbar = document.getElementById("navbar");
+    this.sticky = this.navbar.offsetTop;
+    
+
   }
 
+  @HostListener('window:scroll', []) onWindowScroll() {
+    if(this.navbar != null ){
+      const verticalOffset = window.pageYOffset || document.body.scrollTop || 0;
+      if(verticalOffset > this.sticky){
+        this.navbar.classList.add('sticky');
+      } else {
+        this.navbar.classList.remove('sticky');
+      }
+    }
+  }
 }
